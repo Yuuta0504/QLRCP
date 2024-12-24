77e6ac81829819cfe515efecf8c315417723be98
XxDdUu <tranquangduy15122006@gmail.com>
2024-12-24 21:24:16 +0700

    Update ThanhVien.java

--- src/model/ThanhVien.java
+++ src/model/ThanhVien.java
@@ -3,5 +3,37 @@ package model;
 public class ThanhVien {
 	private String MATv;
 	private String HoTen;
-	private 
+	private String GioiTinh;
+	private String LoaiDoiTuong;
+	public ThanhVien(String mATv, String hoTen, String gioiTinh, String loaiDoiTuong) {
+		super();
+		MATv = mATv;
+		HoTen = hoTen;
+		GioiTinh = gioiTinh;
+		LoaiDoiTuong = loaiDoiTuong;
+	}
+	public String getMATv() {
+		return MATv;
+	}
+	public void setMATv(String mATv) {
+		MATv = mATv;
+	}
+	public String getHoTen() {
+		return HoTen;
+	}
+	public void setHoTen(String hoTen) {
+		HoTen = hoTen;
+	}
+	public String getGioiTinh() {
+		return GioiTinh;
+	}
+	public void setGioiTinh(String gioiTinh) {
+		GioiTinh = gioiTinh;
+	}
+	public String getLoaiDoiTuong() {
+		return LoaiDoiTuong;
+	}
+	public void setLoaiDoiTuong(String loaiDoiTuong) {
+		LoaiDoiTuong = loaiDoiTuong;
+	}
 }
