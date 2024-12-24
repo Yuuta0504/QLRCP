34edd0a3c87530cad864d3c1917fd95801997116
XxDdUu <tranquangduy15122006@gmail.com>
2024-12-24 22:35:39 +0700

    Create Constant.java

---
+++ src/dao/Constant.java
@@ -0,0 +1,7 @@
+package dao;
+
+public class Constant {
+	public static final String String_DB_Url = "jdbc:sqlserver://ADMIN\\\\SQLEXPRESS:1433;databaseName=QLRCP;encrypt=true;trustServerCertificate=true;";
+	public static final String username = "sa";
+	public static final String password = "duy15122006";
+}
